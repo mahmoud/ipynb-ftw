90186781d40248d6be9edf1032125d9070455c1c
Mahmoud Hashemi <makuro@gmail.com>
2012-01-06 17:42:15 -0800

    autocommit  5:42pm  January  6, 2012

--- attr_access.py
+++ attr_access.py
@@ -3,4 +3,24 @@
 
 # <codecell>
 
+class A(object):
+    def __getattribute__(self, name):
+        print 'called getattribute'
+        return object.__getattribute__(self, name)
+        
+a = A()
+print a.__class__
+print type(a)
+
+# <codecell>
+
+# stop_import_stop_import
+cur_file_str = open(__file__, 'r').read()
+from timeit import timeit
+
+# <codecell>
+
+import sys
+import attr_access
+inspect.getsource(sys.modules['attr_access'])
 
