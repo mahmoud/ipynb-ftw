a457c11600c03090d53183a0c580233177c9fd1d
Mahmoud Hashemi <makuro@gmail.com>
2012-01-10 00:55:20 -0800

    autocommit 12:55am  January 10, 2012

--- blocks.py
+++ blocks.py
@@ -11,6 +11,39 @@ def nonlocal_var():
 
 # <codecell>
 
+# debunking http://code.activestate.com/recipes/439096-get-the-value-of-a-cell-from-a-closure/?in=user-2551140
+
+def get_cell_value(cell):
+    return type(lambda: 0)(
+        (lambda x: lambda: x)(0).func_code, {}, None, None, (cell,)
+    )()
+    
+def get_cell_value_mh(cell):
+    return cell.cell_contents
+
+# test
+
+def make_list_appender(mylist):
+    def append_to_mylist(newval):
+        mylist.append(newval)
+        return newval
+    return append_to_mylist
+
+somelist = []
+somelist_appender = make_list_appender(somelist)
+somelist_appender(2)
+somelist_appender(3)
+print somelist
+
+cell = somelist_appender.func_closure[0]
+print get_cell_value(cell)
+print get_cell_value_mh(cell)
+
+print get_cell_value(cell) is somelist
+print get_cell_value_mh(cell) is somelist
+
+# <codecell>
+
 #print nonlocal_var().func_globals
 print dir(nonlocal_var().func_closure[0])
 print nonlocal_var().func_closure[0]
