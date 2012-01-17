fa066e3acf2c9b3562bc95da5e626dfbd8193820
Mahmoud Hashemi <makuro@gmail.com>
2012-01-16 19:34:25 -0800

    autocommit  7:34pm  January 16, 2012

--- kurt-abstract algebra.py
+++ kurt-abstract algebra.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 # <nbformat>2</nbformat>
 
+# <markdowncell>
+
+# <a href="http://www.amazon.com/Book-Abstract-Algebra-Second-Mathematics/dp/0486474178">A Book of Abstract Algebra, second edition</a> Practice Problems
+
+# <markdowncell>
+
+# Chapter 9
+# =========
+# 
+# 9-A: Isomorphism is an Equivalence Relation among Groups
+# --------------------------------------------------------
+# For all isomorphisms:
+# 
+# 1.  Every group is isomorphic to itself  \pm  \breve{a}
+# 2.  If G \simeq G then G \simeq G  \alpha
+# 3.  
+# 
+
+# <codecell>
+
+
 # <codecell>
 
 
