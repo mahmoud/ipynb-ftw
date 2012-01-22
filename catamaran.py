95d5cb53c0ab939497b523c8d18ca5622287fef7
Mahmoud Hashemi <makuro@gmail.com>
2012-01-21 16:23:21 -0800

    autocommit  4:23pm  January 21, 2012

--- catamaran.py
+++ catamaran.py
@@ -3,4 +3,31 @@
 
 # <codecell>
 
+from collections import namedtuple
+from itertools import islice
+
+# <codecell>
+
+f = open('/home/makuro/catamaran/data/simplewiki_categorylinks.csv')
+csv = f.read()
+cat_lines = csv.split('\n')
+print cat_lines[0]
+print cat_lines[1]
+
+CatLink = namedtuple('CatLink', 'from_id to_name link_type')
+
+cat_links = []
+for line in islice(cat_lines, 1, None):
+    if not line:
+        continue
+    fields = line.split('\t')
+        
+    from_id   = fields[0]
+    to_name   = fields[1]
+    sortkey   = fields[2] # probably ignore
+    link_type = fields[6]
+
+    cat_links.append(CatLink(from_id, to_name, link_type))
+    
+print cat_links[0]
 
