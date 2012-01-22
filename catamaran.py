2a52cbe090a8915c214686091b01128c088d48d4
Mahmoud Hashemi <makuro@gmail.com>
2012-01-21 16:40:10 -0800

    autocommit  4:40pm  January 21, 2012

--- catamaran.py
+++ catamaran.py
@@ -8,13 +8,33 @@ from itertools import islice
 
 # <codecell>
 
-f = open('/home/makuro/catamaran/data/simplewiki_categorylinks.csv')
-csv = f.read()
-cat_lines = csv.split('\n')
-print cat_lines[0]
-print cat_lines[1]
-
 CatLink = namedtuple('CatLink', 'from_id to_name link_type')
+
+f = open('/home/makuro/catamaran/data/simplewiki_categorylinks.csv')
+cat_link_csv = f.read()
+cat_lines = cat_link_csv.split('\n')
+
+# <codecell>
+
+f = open('/home/makuro/catamaran/data/simplewiki_page.csv')
+page_csv = f.read()
+page_lines = page_csv.split('\n')
+
+print page_lines[0]
+print page_lines[1]
+print page_lines[2]
+print
+print repr(page_lines[0])
+print repr(page_lines[1])
+
+# <codecell>
+
+namespaces = set(line.split('\t')[1] for line in islice(page_lines, 1, None) if line)
+print namespaces
+
+line.split('\t')[1] for line in islice(page_lines, 1, None) if line
+
+# <codecell>
 
 cat_links = []
 for line in islice(cat_lines, 1, None):
