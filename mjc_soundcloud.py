781900478a2dcd66d5c2d27866f031459929a3fb
Mahmoud Hashemi <makuro@gmail.com>
2012-02-16 21:50:37 -0800

    autocommit  9:50pm  February 16, 2012

--- mjc_soundcloud.py
+++ mjc_soundcloud.py
@@ -3,94 +3,14 @@
 
 # <codecell>
 
-#
-# Conway's thing to pull some stuff from SoundCloud
-#
-
-import lxml
-import lxml.html
-import lxml.etree
-
-# get the raw HTML (from newegg for the first 100 laptops that are listed)
-soundcloudsite = lxml.html.parse( "http://soundcloud.com/groups/top-music-players/tracks?page=1" )
-
-#refers to items on soundcloud page
-songs = soundcloudsite.xpath( '//*[@class="player"]' )
-
-#name of song
-name_text = []
-for song in songs:
-    name = song.xpath( '*[@class="medium mode player"]/*[@class="info-header"]/h3[0]/a' )
-
-    if name:
-        name_text.append( name[ 0 ].text )
-    else:
-        name_text.append("0")
-import pdb; pdb.set_trace()
-#genre of song
-genre_text = []
-for song in songs:
-    genre = song.xpath( '*[@class="medium mode player"]/*[@class="actionbar"]/a' )
-
-    if len( song ) == 1:
-        genre_text.append( genre[ 0 ].text )
-    elif len( song ) == 0:
-        genre_text.append("0")
-
-#number of plays of song
-play_text = []
-for song in songs:
-    play = song.xpath( '*[@class="medium mode player"]/*[@class="info-header"]/*[@class="meta-data"]/*[@class="stats"]/*[@class="plays first"]' )
-    if len( song ) == 1:
-        play_text.append( play[ 0 ].text )
-    elif len( song ) == 0:
-        play_text.append("0")
-
-#number of hearts of song
-fav_text = []
-for song in songs:
-    fav = song.xpath( '*[@class="medium mode player"]/*[@class="info-header"]/*[@class="meta-data"]/*[@class="stats"]/*[@class="comments"]' )
-
-    if len( song ) == 1:
-        fav_text.append( fav[ 0 ].text )
-    elif len( song ) == 0:
-        fav_text.append("0")
-
-#number of downloads of song
-download_text = []
-for song in songs:
-    download = song.xpath( '/*[@class="medium mode player"]/*[@class="info-header"]/*[@class="meta-data"]/*[@class="stats"]/*[@class="favoritings"]' )
-
-    if len( song ) == 1:
-        download_text.append( download[ 0 ].text )
-    elif len( song ) == 0:
-        download_text.append("0")
-
-#number of comments of song
-comment_text = []
-for song in songs:
-    comment = song.xpath( '*[@class="medium mode player"]/*[@class="info-header"]/*[@class="meta-data"]/*[@class="stats"]/*[@class="downloads"]' )
-
-    if len( song ) == 1:
-        comment_text.append( comment[ 0 ].text )
-    elif len( song ) == 0:
-        comment_text.append("0")
+import requests, json
+from pprint import pprint
 
 
+CLIENT_ID = '4c2d540368cee6f46196a41e6bf67255'
 
-# make sure the text data is in ascii format instead of unicode (why do we do this?)
-name_text   = [ text.encode( 'ascii', 'ignore' ) for text in name_text ]
-genre_text   = [ text.encode( 'ascii', 'ignore' ) for text in genre_text ]
-play_text = [ text.encode( 'ascii', 'ignore' ) for text in play_text ]
-fav_text   = [ text.encode( 'ascii', 'ignore' ) for text in fav_text ]
-download_text  = [ text.encode( 'ascii', 'ignore' ) for text in download_text ]
-comment_text  = [ text.encode( 'ascii', 'ignore' ) for text in comment_text ]
+t_json = requests.get('http://api.soundcloud.com/tracks.json?client_id='+CLIENT_ID+'&limit=10')
+tracks = json.loads(t_json.text)
 
-# zip the text lists into a tuple (define:tuple = a list/set of numbers in mathematics and computing)
-tuples = zip( name_text, genre_text, play_text, fav_text, download_text, comment_text )
-
-# write results to a TXT file (created TXT file before running script)
-with open( "../raw_data.tsv", "w" ) as txt_file:
-    for tuple in tuples:
-        txt_file.write( "%s \t %s \t %s \t %s \t %s \t %s \n" % tuple )
+pprint([(track['id'], track['title']) for track in tracks])
 
