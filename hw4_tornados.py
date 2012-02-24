b00be883d8e837b20d8dff3ba6475dd6e6c4a123
Mahmoud Hashemi <makuro@gmail.com>
2012-02-23 21:52:13 -0800

    autocommit  9:52pm  February 23, 2012

--- hw4_tornados.py
+++ hw4_tornados.py
@@ -32,13 +32,13 @@ Nader = namedtuple('Nader', 'lons, lats, fujita, color, size')
 
 # add color to fujita scale
 #DEFAULT_COLOR = "#008020"
-fujita_scale_color_map = { "0": (0.0,0.0,0.75),
-                 "1": (.250,.000,.500),
-                 "2": (.500,.000,.250),
-                 "3": (.500,.000,.000),
-                 "4": (.750,.000,.000),
-                 "5": (.750,.250,.000),
-                    }
+fujita_scale_color_map = { "0": "#0000AA", #(0.0,0.0,0.75),
+                 "1": "#5500AA",#(.250,.000,.500),
+                 }#"2": (.500,.000,.250),
+                 #"3": (.500,.000,.000),
+                 #"4": (.750,.000,.000),
+                 #"5": (.750,.250,.000),
+                 #   }
 
 torns = []
 for t in tuples:
@@ -46,8 +46,8 @@ for t in tuples:
     torns.append(Nader( lons   = float(t[16]),
                         lats  = float(t[15]), 
                         fujita  = fujita,
-                        color   = cc.to_rgb(fujita_scale_color_map.get(fujita, (0.5,0.5,0))),
-                        size   = 3
+                        color   = fujita_scale_color_map.get(fujita, "#000000"),
+                        size   = float(t[21])*float(t[22])
                     ))
 
 # <codecell>
@@ -67,10 +67,12 @@ map_func.drawmapboundary()
 lons = [ t.lons for t in torns ]
 lats = [ t.lats for t in torns ]
 colors = [t.color for t in torns]
+sizes = [float(t.fujita) * 10 for t in torns]
 x,y = map_func(lons, lats)
-colors = ['blue']*(len(x)/2) + ['pink']*(len(x)/2)
-print x[:10], y[:10], colors[:10]
-map_func.scatter(x, y, s=4, marker='o', c=colors, zorder=2, alpha=0.2)
+#colors = ['blue']*(len(x)/2) + ['pink']*(len(x)/2)
+print x[:10], y[:10], colors[:10], sizes[:10]
+from matplotlib import cm
+map_func.scatter(x, y, s=4, marker='o', c=colors, cmap=cm.cool, zorder=2, alpha=0.2)
 
 plt.show()
 
