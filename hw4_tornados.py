5f4c6e3af250a9bd075b3ec17d5d1eaea434d5e7
Mahmoud Hashemi <makuro@gmail.com>
2012-02-23 21:39:35 -0800

    autocommit  9:39pm  February 23, 2012

--- hw4_tornados.py
+++ hw4_tornados.py
@@ -32,7 +32,7 @@ Nader = namedtuple('Nader', 'lons, lats, fujita, color, size')
 
 # add color to fujita scale
 #DEFAULT_COLOR = "#008020"
-fujita_scale_color_map = { "0": (.000,.000,.750),
+fujita_scale_color_map = { "0": (0.0,0.0,0.75),
                  "1": (.250,.000,.500),
                  "2": (.500,.000,.250),
                  "3": (.500,.000,.000),
@@ -46,7 +46,7 @@ for t in tuples:
     torns.append(Nader( lons   = float(t[16]),
                         lats  = float(t[15]), 
                         fujita  = fujita,
-                        color   = cc.to_rgba(fujita_scale_color_map.get(fujita, (0,0,0))),
+                        color   = cc.to_rgb(fujita_scale_color_map.get(fujita, (0.5,0.5,0))),
                         size   = 3
                     ))
 
@@ -64,10 +64,13 @@ map_func.drawstates(color = 'gray')
 map_func.fillcontinents(color = 'beige')
 map_func.drawmapboundary()
 
-lons = [ l.lons for l in torns ]
-lats = [ l.lats for l in torns ]
+lons = [ t.lons for t in torns ]
+lats = [ t.lats for t in torns ]
+colors = [t.color for t in torns]
 x,y = map_func(lons, lats)
-map_func.plot(x, y,'o', c=(.270,.459,.706), alpha=.1)
+colors = ['blue']*(len(x)/2) + ['pink']*(len(x)/2)
+print x[:10], y[:10], colors[:10]
+map_func.scatter(x, y, s=4, marker='o', c=colors, zorder=2, alpha=0.2)
 
 plt.show()
 
