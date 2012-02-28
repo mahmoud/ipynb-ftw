7e61f2e4b74a94a4ba2e41b03c4a3497c40302db
Mahmoud Hashemi <makuro@gmail.com>
2012-02-27 20:37:51 -0800

    autocommit  8:37pm  February 27, 2012

--- mjc_lastfm.py
+++ mjc_lastfm.py
@@ -3,4 +3,40 @@
 
 # <codecell>
 
+# hw5 cs171
+
+import requests, json
+from pprint import pprint
+
+#api account information
+api_key = '8556b3ac919e1575cd05f49fb9d6fe08'
+secret = '9ed0596f7ad4ceae66bac1561d1b5451'
+
+#request authentification
+t_json_1 = requests.get('http://ws.audioscrobbler.com/2.0/?method=chart.gethypedtracks&api_key='+api_key+'&limit=2&format=json')
+hyped_tracks = json.loads(t_json_1.text)['tracks']['track']
+
+[(t['artist']['name'], t['name']) for t in hyped_tracks]
+
+# <codecell>
+
+#get names for hyped tracks
+hyped = str([(track['name']) for track in hyped_tracks])
+
+t_json_2 = requests.get('http://ws.audioscrobbler.com/2.0/?method=track.getinfo&api_key='+api_key+'&artist=cher&track='+hyped+'')
+tracks = json.loads(t_json_2.text)
+
+#get information for hyped tracks
+track_text = str([(track['name'], track['listeners'], track['playcount'], track['album position'], track['toptags/tag/name'], track['favoritings_count']) for track in tracks])
+
+#copy information to csv file
+with open( "last_fm_hyped.csv", "w" ) as txt_file:
+    for track in tracks:
+        txt_file.write( track_text )
+
+with open('../tracks.json', 'w') as f:
+    json.dump(tracks, f, indent=2, sort_keys=True)
+
+with open('../tracks.json') as f:
+    tracks = json.load(f)
 
