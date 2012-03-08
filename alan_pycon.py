2caf021fc07cadc5180c23f7a1837b275632d46b
Mahmoud Hashemi <makuro@gmail.com>
2012-03-08 14:16:26 -0800

    autocommit  2:16pm  March  8, 2012

---
+++ alan_pycon.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# <nbformat>2</nbformat>
+
+# <codecell>
+
+class Derp(object):
+    def __init__(self):
+        print 'self.__class__.__name__:',self.__class__.__name__
+        
+d = Derp()
+
+# <codecell>
+
+
