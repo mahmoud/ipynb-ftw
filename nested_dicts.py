25bea6e1a3ce53cc82966aba30ab76a44aa8c26a
Mahmoud Hashemi <makuro@gmail.com>
2012-05-08 22:48:50 -0700

    autocommit 10:48pm  May  8, 2012

--- nested_dicts.py
+++ nested_dicts.py
@@ -5,8 +5,23 @@
 
 import json
 
-with open('weirdnesteddicts.json') as di:
+with open('/home/ipy/notebooks/weirdnesteddicts.json') as di:
     stuff = json.load(di)
 
-print keys(stuff)
+revisions = stuff['revisions']
+
+print revisions.keys()[0]
+print revisions.values()[0]
+
+# <codecell>
+
+from collections import defaultdict
+
+mega_dict = defaultdict(lambda: defaultdict(list))
+
+for rev_date, sec_data in revisions.items():
+    for section, data in sec_data.items():
+        mega_dict[rev_date][section].append(data)
+    
+print mega_dict.values()[0]
 
