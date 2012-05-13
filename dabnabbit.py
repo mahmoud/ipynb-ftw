feaae927ecfce1bafabd4100dbf8d179b0b0ea3f
Mahmoud Hashemi <makuro@gmail.com>
2012-05-12 22:40:04 -0700

    autocommit 10:40pm  May 12, 2012

--- dabnabbit.py
+++ dabnabbit.py
@@ -3,7 +3,17 @@
 
 # <codecell>
 
-#import gevent
+import gevent
+
+from gevent import socket
+urls = ['en.wikipedia.org', 'example.com']
+jobs = [gevent.spawn(socket.gethostbyname, url) for url in urls]
+gevent.joinall(jobs, timeout=2)
+[job.value for job in jobs]
+
+# <codecell>
+
+
 
 import requests
 import json
