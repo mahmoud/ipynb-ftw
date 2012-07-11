ab22e17ff9b6637a3afb93db7742934f39d73c72
Mahmoud Hashemi <makuro@gmail.com>
2012-07-10 21:36:36 -0700

    autocommit  9:36pm  July 10, 2012

--- revision_stats.py
+++ revision_stats.py
@@ -16,16 +16,6 @@ print sorted(revs[0].keys())
 
 # <codecell>
 
-with open('../tracks.json', 'w') as f:
-    json.dump(tracks, f, indent=2, sort_keys=True)
-
-# <codecell>
-
-with open('../tracks.json') as f:
-    tracks = json.load(f)
-
-# <codecell>
-
 sorted(revs, key=lambda x: int(x['rev_id']), reverse=True)[0]
 
 from datetime import timedelta, datetime
@@ -61,42 +51,11 @@ print stats.skew(data)
 
 print stats.describe(data)
 
-plot(range(0,len(data)), data_unsorted)
+plot(data_unsorted)
 
 # <codecell>
 
-import numpy
-import matplotlib.pyplot
-import json
-
-colors = {}
-
-x_val = lambda t: int(t['playback_count'])
-y_val = lambda t: int(t['favoritings_count'])
-c_val = lambda t: colors.get(t['genre'], 'blue')
-s_val = lambda t: 50
-
-x_label = 'Plays'
-y_label = 'Downloads'
-
-print [(x_val(t), y_val(t)) for t in tracks]
-print set(t['genre'] for t in tracks)
-
-props = dict( alpha=0.5, edgecolors = 'none')
-
-plot = matplotlib.pyplot.scatter(
-        [ x_val(t) for t in tracks ],
-        [ y_val(t) for t in tracks ],
-        c=[ c_val(t) for t in tracks ],
-        s=[ s_val(t) for t in tracks ],
-        **props)
-
-matplotlib.pyplot.title( "SoundCloud tracks" )
-matplotlib.pyplot.xlabel( x_label )
-matplotlib.pyplot.ylabel( y_label )
-matplotlib.pyplot.grid( True, color = "yellow", which = 'both' )
-
-matplotlib.pyplot.show()
+plot(data)
 
 # <codecell>
 
